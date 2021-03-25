80127165df5b2187de1aed60f162a19a25f42d14
duclayan <duclayandominique@gmail.com>
2021-03-25 16:51:28 +0400

    Main Code

--- aircraftproblem.rb
+++ aircraftproblem.rb
@@ -14,38 +14,46 @@
         end
 
         def arange_seat_template(location,columnSize, rowSize)
-            max_columnsize = @total_column + columnSize
-
+            max_columnSize = @total_column + columnSize
+            # Records the (W)indow Seat and the (A)isle 
+            # Seats are labeled as "1-A", "2-W", and "3-M" for easier sorting
+            #Formulas Used: 
+                    # 1 => C = 1 assumes first coordinate is always a window chair
+                    # columnSize=> takes the last column coordinate as an aisle chair
+                    # max_columnSize =>assumes that max_columnSize is the current last column
+                    # (@total_column + 1) => @total_column + 1 give the coordinate of the first chair
+                    # (@total_column + 2)  => Used to indicate the chair next to the aisle
+                    # (max_columnSize - 1) => generate the last chair before the current last column
+            for row in 1 .. rowSize
+                if location == 'left'
+                    @add_to_record.push([row, 1, "2-W"], [row, columnSize, "1-A"])
+                elsif location == 'right'
+                    @add_to_record.push([row, (@total_column + 1), "1-A"], [row, max_columnSize, "2-W"])
+                elsif location == "middle"
+                    @add_to_record.push([row, (@total_column + 1), "1-A"], [row, max_columnSize, "1-A"])
+                end
+            end
+            # Check item has middle seats
+            if columnSize > 2
                 for row in 1 .. rowSize
-                    # Records the (W)indow Seat and the (A)isle 
-                    if location == 'left'
-                        @add_to_record.push([row, 1, "2-W"], [row, columnSize, "1-A"])
-                    elsif location == 'right'
-                        @add_to_record.push([row, (@total_column + 1), "1-A"], [row, max_columnsize, "2-W"])
-                    elsif location == "middle"
-                        @add_to_record.push([row, (@total_column + 1), "1-A"], [row, max_columnsize, "1-A"])
+                    for column in (@total_column + 2) .. (max_columnSize - 1 )
+                        @add_to_record.push([row,column, "3-M"])
                     end
                 end
-                    # Check item has middle seats
-                    if columnSize > 2
-                        for row in 1 .. rowSize
-                            for column in (@total_column + 2) .. (@total_column + (columnSize - 1 ))
-                                @add_to_record.push([row,column, "3-M"])
-                            end
-                        end
-                    end
+            end
             sort_record()
             @total_column += columnSize
         end
 
         def sort_record ()
-            @add_to_record = @add_to_record.sort_by{|row,column,location| [ row]}
-            @add_to_record = @add_to_record.sort_by{|row,column,location| [ location.chars.first.chomp.to_i]}
+            # 1. Sort record by row
+            # 2. then sort it according to the seat label (W,  M,  A)
+            @add_to_record = @add_to_record.sort_by{|row,column,location| [row]}
+            @add_to_record = @add_to_record.sort_by{|row,column,location| [location.chars.first.chomp.to_i]}
 
         end
 
         def allocate_passenger_seat
-            sort_record()
             for i in 0 .. (@passenger - 1)
                 @passenger_seat[i] = @add_to_record[i]
             end
@@ -55,16 +63,52 @@
             puts "add to record #{@add_to_record}"
             puts "Passengers: #{@passenger_seat}"
         end
+
+        def show_passenger
+            return @passenger_seat
+        end
     end
 
-    newFlight = Aircraft.new(4, 30)
-    newFlight .arange_seat_template('left',3,2)
-    newFlight .arange_seat_template('middle',4,3)
-    newFlight .arange_seat_template('middle',2,3)
-    newFlight .arange_seat_template('right',3,4)
-    newFlight .allocate_passenger_seat()
-    newFlight.show_results()    
+    # UNCOMMENT THIS SECTION FOR: Auto generated input section
+    newFlightPlan = Aircraft.new(4, 30)
+    newFlightPlan.arange_seat_template('left',3,2)
+    newFlightPlan.arange_seat_template('middle',4,3)
+    newFlightPlan.arange_seat_template('middle',2,3)
+    newFlightPlan.arange_seat_template('right',3,4)
 
+
+    # # Uncomment this for user to be able to enter own input
+    # puts "Enter the number of columns:"
+    # cell = gets.chomp.to_i
+    # puts "Enter the number of passengers:"
+    # passenger = gets.chomp.to_i
+
+    # newFlightPlan = Aircraft.new(cell,passenger)
+
+    # # Loop to get the rows and columns per cell 
+    # for i in 1 .. cell 
+
+    #     puts "Enter Column"
+    #     colSize = gets.chomp.to_i
+    #     puts "Enter Row"
+    #     rowSize = gets.chomp.to_i
+
+
+    #     if i == 1
+    #         location = 'left'
+    #     elsif i == cell
+    #         location = 'right'
+    #     else
+    #         location = 'middle'
+    #     end
+    #     newFlightPlan.arange_seat_template(location,colSize, rowSize)
+    # end
+
+    # # Do not comment out the next two lines
+
+    newFlightPlan.allocate_passenger_seat()
+    newFlightPlan.show_results()  
+    
 
 
 
--- spec.rb
+++ spec.rb
@@ -4,7 +4,8 @@ require_relative 'aircraftproblem'
 describe "Aircraft" do
     
     before do
-        @flight = Aircraft.new(:cell => 4,:passenger =>30)
+        @flight = Aircraft.new( 4,30)
+        @passenger_seat =[]
     end
 
     # it "" do
@@ -15,6 +16,28 @@ describe "Aircraft" do
         assert_operator @flight.cell, :>=, 1
     end
 
-    
+    it "is not valid without cell" do
+        # @newFlight = Aircraft.new(nil,nil)
+        assert @flight != nil
+    end
+
+    it "should be an array" do
+        newFlightPlan = Aircraft.new(4, 30)
+        newFlightPlan.arange_seat_template('left',3,2)
+        newFlightPlan.arange_seat_template('middle',4,3)
+        newFlightPlan.arange_seat_template('middle',2,3)
+        newFlightPlan.arange_seat_template('right',3,4)
+
+        newFlightPlan.allocate_passenger_seat()
+        assert newFlightPlan.show_passenger() == [[1, 3, "1-A"], [1, 4, "1-A"], [1, 7, "1-A"], [1, 8, "1-A"], [1, 9, "1-A"], [1, 10, "1-A"], [2, 3, "1-A"], [2, 4, "1-A"], [2, 7, "1-A"], [2, 8, "1-A"], [2, 9, "1-A"], [2, 10, "1-A"], [3, 4, "1-A"], [3, 7, "1-A"], [3, 8, "1-A"], [3, 9, "1-A"], [3, 10, "1-A"], [4, 10, "1-A"], [1, 1, "2-W"], [1, 12, "2-W"], [2, 1, "2-W"], [2, 12, "2-W"], [3, 12, "2-W"], [4, 12, "2-W"], [1, 2, "3-M"], [1, 5, "3-M"], [1, 6, "3-M"], [1, 11, "3-M"], [2, 2, "3-M"], [2, 5, "3-M"]]
+    end
+
+    it "returns and error when input not valid" do 
+        @invalid_id = -1
+        assert_raises RuntimeError do
+            @flight = Aircraft.new(@invalid_id, 20)
+        end
+    end
+
 
 end
